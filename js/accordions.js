09296595909351452282d737f240124e39b732d8
Marc Molta <mcmolta10@gmail.com>
2018-07-25 11:36:27 -0400

    split the accordions snippet into the appropriate directories + added its css

--- js/accordions.js
+++ js/accordions.js
@@ -1,29 +1,11 @@
-/********** Accessible Accordion(s) Functionality **********/
+/* ---------- */
+/* Accessible Accordion(s) Functionality */
+/* ---------- */
+
+/* see accordions.html for structure & accordions.css for styling */
 
 
-/* This function assumes the following HTML Structure:
-
-    <ul aria-label="Accordion Control Group Button" class="accordion-controls">
-        <li>
-            <button aria-controls="content-1" aria-expanded="false" id="accordion-controls-1" class="accordion"></button>
-            <div aria-hidden="true" id="content-1" class="panel">
-                
-                // Accordion Content Goes Here
-
-            </div>
-        </li>
-    </ul>
-
-    ^FOR PAGES WITH MULTIPLE ACCORDTIONS, remember to update the following elements:
-        <button>:
-            aria-controls="content-n"
-            id="accordion-controls-n"
-        <div>:
-            id="content-n"
-
-*/
-
-// Function for pages w/1 accordion. See lines 54-60 for how to handle pages w/multiple accordions
+// Function for pages w/1 accordion. See lines 35-42 for how to handle pages w/multiple accordions
 const accordion = document.querySelector('.accordion')
 
 accordion.onclick = function(){
@@ -50,12 +32,12 @@ accordion.onclick = function(){
     }
 }
 
-// FOR PAGES WITH MULTIPLE ACCORDIONS, replace lines 27 & 29 with:
+// FOR PAGES WITH MULTIPLE ACCORDIONS, replace lines 9 & 11 with:
 const accordions = document.querySelectorAll('.accordion') 
 
 for(var i = 0; i < accordions.length; i++){
     accordions[i].onclick = function(){
-        // same function as lines 30-50
+        // same function as lines 11-31
     }
 }
 
