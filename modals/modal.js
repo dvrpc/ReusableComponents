36715e267209916d5b4ec12cbb090706eacb74b0
Marc Molta <mcmolta10@gmail.com>
2018-12-04 10:18:44 -0500

    add accessible modal snippets

---
+++ modals/modal.js
@@ -0,0 +1,41 @@
+/******************* Accessible Modal *******************/
+// @TODO: update with tab Index stuff to round out the accessibility of this modal
+
+var modal = document.querySelector('#modal')
+var modalToggle = document.querySelector('#modal-toggle')
+var close = document.querySelector('#close-modal')
+
+// hide and add aria-hidden attribute
+var ariaHideModal = function() {
+  modal.style.display = 'none'
+  modal.setAttribute('aria-hidden', 'true')
+}
+
+// reveal and remove aria-hidden attribute
+var ariaShowModal = function() {
+  modal.style.display = 'block'
+  modal.setAttribute('aria-hidden', 'false')
+}
+
+// open the modal by clicking the div
+modalToggle.onclick = function(){
+  modal.style.display = 'none' ? ariaShowModal() : ariaHideModal()
+}
+
+// closing the modal options: by clicking the 'x' or anywhere outside of it or pressing the escape key
+close.onclick = function(){ariaHideModal()}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      ariaHideModal()
+    }
+}
+document.onkeydown = function(event) {
+  // make sure the modal is open first
+  if(modal.style.display === 'block'){
+    // keyCode for the escape key
+    if(event.keyCode === 27){
+      ariaHideModal()
+    }
+  }
+}
