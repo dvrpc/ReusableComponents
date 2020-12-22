97200eb5ea635d5d2ae0316fc6a2a7600fa3f7a7
mmolta <mcmolta10@gmail.com>
2020-12-22 10:50:59 -0500

    update non-framework webpack config

--- config-files/webpack-for-non-js.config.js
+++ config-files/webpack-for-non-js.config.js
@@ -1,40 +1,40 @@
 /* 
-    Steps to create production bundles for apps with older* architecture
-        See webpack.config.js for projects built in React, ES6 modules or similar
-        * This does still assume modular js w/a single entry point (i.e. index.js). Single file behemoths also work.
+    Steps to create production bundles for apps with traditional* architecture
+        See webpack.config.js for projects built in React etc
+        * This does still assume modular js w/a single entry point (i.e. index.js). Monorepos also work.
     
     The output from these steps are:
-        A minified, polyfilled production bundle for your javascript
+        A minified, (polyfilled) production bundle for your javascript
         Minified HTML pages 
-        (tbd) Minified CSS
+        Minified CSS
         The rest of your static assets
 
     DEV-DEPENDENCIES (npm i --save-dev):
-        path
-        @babel/core
-        @babel/preset-env
-        babel-loader
-        babel-preset-minify
-        copy-webpack-plugin
-        html-webpack-plugin
-        extract-text-webpack-plugin
-        css-loader
-        file-loader
-        style-loader
-        webpack
-        webpack-cli
+        "copy-webpack-plugin": "^7.0.0",
+        "css-loader": "^5.0.1",
+        "css-minimizer-webpack-plugin": "^1.1.5",
+        "file-loader": "^6.2.0",
+        "html-webpack-plugin": "^4.5.0",
+        "mini-css-extract-plugin": "^1.3.3",
+        "path": "^0.12.7",
+        "style-loader": "^2.0.0",
+        "webpack": "^5.10.3",
+        "webpack-cli": "^4.2.0"
         
-    DEPENDENCIES (npm i)
-        @babel/polyfill
+    DEPENDENCIES (npm i) if necessary
+        @babel/polyfill 
 */
 
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
 
 
 /***** Steps to copy and minify existing HTML files using html-webpack-plugin *****/
-
 // Use this for individual files (i.e. index.html)
 let indexConfig = new HtmlWebpackPlugin({
     template: path.resolve(__dirname + "/index.html"),
@@ -50,8 +50,7 @@ let indexConfig = new HtmlWebpackPlugin({
         useShortDoctype: true
     }
 })
-
-// If a folder has subdirectories with multiple html files, use this process to map over each one 
+// OTHERWISE if a folder has subdirectories with multiple html files, use this process to map over each one 
 const otherHTML = [
     'the',
     'name',
@@ -61,15 +60,10 @@ const otherHTML = [
     'without',
     '.html extension'
 ]
-
-const otherHTMLConfig = otherHTML.map(html => {
+const multipleHTMLConfig = otherHTML.map(html => {
     return new HtmlWebpackPlugin({
         filename: html + '.html',
         template: path.resolve(__dirname + `/path/to/the/file/${html}.html`),
-        /*
-        Use this step at your discretion
-        inject: body,
-        */
         minify: {
             collapseWhitespace: true,
             removeComments: true,
@@ -80,10 +74,13 @@ const otherHTMLConfig = otherHTML.map(html => {
         }
     })
 })
+/***** END bundling HTML *****/
 
 
-/***** Bundle + Static Assets creation *****/
+
+/***** JS/CSS Bundle + Static Assets creation *****/
 module.exports = {
+    // note: you can just have './js/index.js' if babel isn't needed but it must still be in an array
     entry: ['@babel/polyfill', './js/index.js'],
     mode: 'production',
     module: {
@@ -91,11 +88,49 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
+                
+                // remove if not using babel
                 loader: 'babel-loader',
                 query: {
                     presets: ['minify']
                 }
+            },
+            // load styles
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            // load imgs
+            {
+                // cover all test cases (do you have .jpg or otherwise? add to test case)
+                test: /\.(png|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]'
+                        }
+                    }
+                ]
             }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            })
         ]
     },
     output: {
@@ -103,24 +138,26 @@ module.exports = {
         path: path.resolve(__dirname, 'build')
     },
     plugins: [
-        // CopyWebpackPlugin moves assets into the build folder and lets you determine if they are directories or files
+        // CopyWebpackPlugin moves assets into the build folder
         // we use it here for the img & css folders b/c this set up assumes you aren't using 'require' or 'import' to load either asset
-        new CopyWebpackPlugin(
-            [
+        new CopyWebpackPlugin({
+            patterns: [
                 {
                     from: './img',
                     to: 'img',
                     toType: 'dir'
                 },
-                // @TODO: replace this with extract-text-webpack-plugin config
                 {
                     from: './css',
                     to: 'css',
                     toType: 'dir'
                 }
             ]
-        ),
-    // HtmlWebpackPlugin
+        }),
+        // add HTML bundle here IF only HTML file is index
         indexConfig
-    ].concat(otherHTMLConfig)
+    ]
+    // add HTML bundle here IF multiple HTML files 
+    //].concat(multipleHTMLConfig)
 }
+/***** END bundle *****/
