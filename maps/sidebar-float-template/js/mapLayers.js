c6f3f604e4b0c85c942770644131aa9bad366d3b
mmolta <mcmolta10@gmail.com>
2021-02-03 14:53:01 -0500

    update map center + zoom for full width & change outline colors for dark theme

--- maps/sidebar-float-template/js/mapLayers.js
+++ maps/sidebar-float-template/js/mapLayers.js
@@ -6,7 +6,7 @@ const layers = {
         "source-layer": "county",
         "paint": {
             'line-width': 2.5,
-            'line-color': '#505a5e'
+            'line-color': '#f7f7f7'
         },
         "filter": [
             "==",
@@ -21,7 +21,7 @@ const layers = {
         "source-layer": "municipalities",
         "paint": {
             'line-width': 0.5,
-            'line-color': '#4a5c64'
+            'line-color': '#e7e7e7'
         }
     }
     // add more layers here
