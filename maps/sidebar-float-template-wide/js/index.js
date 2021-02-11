86c60b35686535d8ea57077ec1bfc9c3aa47515e
mmolta <mcmolta10@gmail.com>
2021-02-11 17:52:36 -0500

    create toggleLayer fnc and apply to map

--- maps/sidebar-float-template-wide/js/index.js
+++ maps/sidebar-float-template-wide/js/index.js
@@ -2,10 +2,14 @@ import makeMap from './map.js'
 import sources from './mapSources.js'
 import layers from './mapLayers.js'
 import { ariaShowModal, ariaHideModal } from './modal.js'
+import { toggleLayers } from './forms.js'
 
 const modal = document.getElementById('modal')
 const modalToggle = document.getElementById('modal-toggle')
 const closeModal = document.getElementById('close-modal')
+
+// OPTIONAL elements. Uncomment to use
+const toggleLayerForms = Array.from(document.querySelectorAll('.sidebar-form-toggle'))
 
 // map
 const map = makeMap()
@@ -13,6 +17,9 @@ const map = makeMap()
 map.on('load', () => {
     for(const source in sources) map.addSource(source, sources[source])
     for(const layer in layers) map.addLayer(layers[layer])
+
+    // OPTIONAL functionality. Uncomment to use
+    toggleLayerForms.forEach(form => toggleLayers(form, map))
 })
 
 
